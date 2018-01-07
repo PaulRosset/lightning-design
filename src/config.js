00227eee93f9a3c366de33ab1bb90c798a4e62de
Paul Rosset <paulrosset96@gmail.com>
2018-01-07 20:44:38 +0100

    Add uid on addUser

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
   url: "http://localhost:8081/graphql",
   client_id: "3132b8f936e031819b70",
-  token_secret: "778f7c99477c49a8f87f6ed6746444a8c3be5015"
+  token_secret: ""
 };
